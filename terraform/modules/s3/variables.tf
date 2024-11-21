f5595c5a7d5ee866b075a569814a6f93b0fc7666
Alexio Junior Aaron <97569992+Ajay263@users.noreply.github.com>
2024-11-21 11:43:05 +0200

    Update variables.tf

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -1,11 +1,11 @@
-# modules/s3/variables.tf
 variable "api_ingestion_bucket_name" {
   description = "Name of the S3 bucket for API data ingestion"
   type        = string
+  default     = "retail-database"
 }
 
 variable "data_lake_bucket_name" {
   description = "Name of the S3 bucket for Data Lake storage"
   type        = string
+  default     = "retail-datalake"
 }
-
